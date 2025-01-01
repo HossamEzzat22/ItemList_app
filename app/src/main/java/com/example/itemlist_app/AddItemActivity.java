387e402f903da88279a5139072f288ef50854efb
Hossam Ezzat <122276796+HossamEzzat22@users.noreply.github.com>
2025-01-01 17:04:58 +0200

    11

--- app/src/main/java/com/example/itemlist_app/AddItemActivity.java
+++ app/src/main/java/com/example/itemlist_app/AddItemActivity.java
@@ -1,21 +1,64 @@
 package com.example.itemlist_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class AddItemActivity extends AppCompatActivity {
+    Toolbar toolbar;
+    EditText editTextName ;
+    EditText editTextPrice;
+    EditText editId;
+    String textName;
+    String textPrice;
+    String id;
+
+
+    Button add;
+    Button cancel;
+
+    Intent intent;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        editTextName = (EditText) findViewById(R.id.itemName);
+        editTextPrice = (EditText) findViewById(R.id.itemPrice);
+        add = (Button) findViewById(R.id.add);
+        cancel = (Button) findViewById(R.id.cancel);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textName=editTextName.getText().toString();
+                textPrice=editTextPrice.getText().toString();
+//                id = editId.getText().toString();
+
+                // Pass the text to the next screen using Intent
+                intent = new Intent(AddItemActivity.this, MainActivity.class);
+                intent.putExtra("input_value_name",textName);
+                intent.putExtra("input_value_price",textPrice);
+//                intent.putExtra("input_value_id",id);
+                startActivity(intent);
+            }
+        });
+
+
+
         setSupportActionBar(toolbar);
 
     }
@@ -28,15 +71,15 @@ public class AddItemActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         // Handle action bar item clicks here.
-        int id = item.getItemId();
-
-        // Handle menu item clicks
-        if (id == R.id.mainMenu) {
-            Toast.makeText(this, "Settings clicked", Toast.LENGTH_SHORT).show();
-            return true;
-        }
+//        int id = item.getItemId();
+//
+//        // Handle menu item clicks
+//        if (id == R.id.mainMenu) {
+//            Toast.makeText(this, "Settings clicked", Toast.LENGTH_SHORT).show();
+//            return true;
+//        }
         return super.onOptionsItemSelected(item);
     }
 
--- app/src/main/java/com/example/itemlist_app/MainActivity.java
+++ app/src/main/java/com/example/itemlist_app/MainActivity.java
@@ -5,36 +5,50 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+import android.widget.TextView;
 
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
+    FloatingActionButton fab;
+    TextView itemName;
+    String recievedItemName;
+    TextView itemPrice;
+    String recievedItemPrice;
+
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        Button button = findViewById (R.id.fab);
-        FloatingActionButton fab = findViewById(R.id.fab);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        itemName = (TextView) findViewById(R.id.tvItemName);
+        itemPrice = (TextView) findViewById(R.id.tvItemPrice);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+
+
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,AddItemActivity.class);
+                Intent intent = new Intent(MainActivity.this, AddItemActivity.class);
                 startActivity(intent);
             }
         });
-
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        recievedItemName= getIntent().getStringExtra("input_value_name");
+        recievedItemPrice= getIntent().getStringExtra("input_value_price");
+        itemName.setText(recievedItemName);
+        itemPrice.setText(recievedItemPrice);
         setSupportActionBar(toolbar);
-
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -43,17 +57,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here.
-        int id = item.getItemId();
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
-        // Handle menu item clicks
-        if (id == R.id.mainMenu) {
-            Toast.makeText(this, "Settings clicked", Toast.LENGTH_SHORT).show();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+//        switch (item.getItemId()) {
+//            case R.id.settings :
+//                startActivity(new Intent(this, AddItemActivity.class));
+//                return true;
+//            case R.id.logout:
+//                startActivity(new Intent(this, LoginActivity.class));
+//                return true;
+//            default:
+//                return super.onOptionsItemSelected(item);
+//        }
+        return false;
     }
-
-
 }
