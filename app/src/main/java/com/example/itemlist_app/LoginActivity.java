49727d8126d1357931514140d0789f37bde8b141
Hossam Ezzat <hossamezzat220@gmail.com>
2025-01-03 14:31:10 +0200

    sf

--- app/src/main/java/com/example/itemlist_app/LoginActivity.java
+++ app/src/main/java/com/example/itemlist_app/LoginActivity.java
@@ -5,10 +5,5 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
 
-    }
 }
